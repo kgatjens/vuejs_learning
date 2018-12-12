7ec6cca464b875f65d089d26d83c9cbcba2223aa
kbrenes <kenneth.brenes@gorillalogic.com>
2018-12-12 08:46:00 -0600

    Added new components

---
+++ cli/vue-shop/src/components/Cart.vue
@@ -0,0 +1,13 @@
+<template>
+    <ul class="list-group">
+        <li class="list-group-item" v-for="item in items" :key="item.id">
+            {{ item.title }} - $ {{ item.price }}
+            <button @click="$emit('remove-from-cart',item)" class="btn badge badge-warning float-right">Remove</button>
+        </li>
+    </ul>
+</template>
+<script>
+export default{
+    props:['items']
+}
+</script>
